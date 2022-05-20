49264cc4af30e47cfa56f0e0a3b204ecfc4bfd39
francodem <francomorenoq@gmail.com>
2022-05-20 07:44:59 -0700

    finished

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
-result = 12 % 12
-print(str(result))
+arr = [1, 2, 1, 4, 5]
+
+def countingSort(arr):
+    # Write your code here
+   
+    zeros = [0]*len(arr)
+    for i in arr:
+        zeros[i-1] += 1 
+    
+    return zeros 
+
+print(countingSort(arr))
